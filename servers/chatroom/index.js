3b46cbdcd25067515b027fed676f7487a7f52292
?¨????? <1915250964@qq.com>
2016-10-05 10:37:36 +0800

    整理webpack配置文件

---
+++ servers/chatroom/index.js
@@ -0,0 +1,92 @@
+import fs from 'fs';
+let socketIO = require('socket.io');
+
+let createFileName = (room, type, fileType = 'txt') => {
+    return `../../chatData/${room}${type}${(new Date()).format('yyyyMMdd')}.${fileType}`
+};
+
+let readData = (data) => {
+    let arr = data.toString().split('\n');
+    let arrData = [];
+    arr.map((item) => {
+        if (!item) return;
+        try {
+            let obj = JSON.parse(item.toString())
+            arrData.push(obj)
+        } catch (e) {
+            console.info('===JSON.parse===');
+            console.info(e)
+        }
+    });
+    return arrData;
+}
+
+export default (server) => {
+    let io = socketIO(server);
+
+    io.on('connection', function (socket) {
+    socket.on('message', (mes) => {
+        if (!socket.name) {
+            return socket.emit('login');
+        }
+        /*socket.to(socket.room).emit('message', {
+         sender: socket.name,
+         cnt: mes.writeMessage,
+         sendTime: new Date()
+         });*/
+        let msg = {
+            sender: socket.name,
+            cnt: mes.writeMessage,
+            sendTime: (new Date()).format('yyyy-MM-dd hh:mm:ss')
+        };
+
+        fs.appendFile(createFileName(socket.room, 'message'), JSON.stringify(msg) + '\n', (err) => {
+            if (err) throw err;
+            io.to(socket.room).emit('message', { message: [msg] });
+        })
+    });
+
+    socket.on('login', (obj) => {
+        socket.name = obj.userName;
+        socket.room = obj.namespace || 'default';
+        socket.join(socket.room);
+        let hasOldMessage = false;
+        let msgFile;
+        try {
+            msgFile = fs.readFileSync(createFileName(socket.room, 'message'));
+            hasOldMessage = readData(msgFile);
+        } catch (e) {
+            fs.appendFileSync(createFileName(socket.room, 'message'), '');
+        }
+        let hasLinkers = false;
+        let usersFile;
+        try {
+            usersFile = fs.readFileSync(createFileName(socket.room, 'users'));
+            hasLinkers = readData(usersFile);
+        } catch (e) {
+            fs.appendFileSync(createFileName(socket.room, 'users'), '');
+        }
+        let users = {
+            name: obj.userName,
+            id: new Date().getTime() + (Math.random() * 10000).toFixed(0)
+        };
+        fs.appendFile(createFileName(socket.room, 'users'), JSON.stringify(users) + '\n', (err) => {
+            if (err) throw err;
+            socket.emit('message', {
+                hasLogin: true,
+                userName: socket.name,
+                linker: hasLinkers || [],
+                message: hasOldMessage || []
+            });
+
+            io.to(socket.room).emit('message', {
+                linker: [users]
+            });
+        });
+    });
+});
+}
+
+
+
+
---
+++ webpack.product.config.js
@@ -0,0 +1,85 @@
+const webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+
+const pkg = require('./package.json');
+const common = require('./webpack.common.config');
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build'),
+    node_modules: path.join(__dirname, 'node_modules')
+};
+// pack into vendors unless server side depend on.
+const whiteLists = [
+    'express',
+    'body-parser',
+    'compression',
+    'ejs'
+]
+
+const vendors = Object.keys(pkg.dependencies).filter((v) => {
+    return whiteLists.indexOf(v) === -1
+})
+
+module.exports = merge(common, {
+    entry: {
+        app: PATHS.app,
+        vendors: vendors
+    },
+    devtool: 'eval-source-map',
+    debug: true,
+    output: {
+        path: PATHS.build,
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[chunkhash].js',
+    },
+    resolve: {
+        alias: {
+            // 'react': path.resolve(PATHS.node_modules, 'react/dist/react.js'),
+            // 'react-dom': path.resolve(PATHS.node_modules, 'react-dom/dist/react-dom.js'),
+            'react-redux': path.resolve(PATHS.node_modules, 'react-redux/dist/react-redux.js'),
+            'react-router': path.resolve(PATHS.node_modules, 'react-router/lib/index.js'),
+            'redux': path.resolve(PATHS.node_modules, 'redux/dist/redux.js')
+        }
+    },
+    node: {
+        console: true, fs: 'empty', tls: 'empty'
+    },
+    module: {
+        // noParse: [
+        //     'ws'
+        // ],
+        loaders: [
+            { test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css!less'), include: PATHS.app },
+            { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css'), include: PATHS.app },
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192', include: PATHS.app },
+            { test: /\.(woff|woff2)$/, loader: 'url-loader?limit=100000', include: PATHS.app }
+        ]
+    },
+    // externals: ['ws'],
+    plugins: [
+        new HtmlwebpackPlugin({
+            template: 'node_modules/html-webpack-template/index.html',
+            title: 'S-B',
+            appMountId: 'app'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({ name: 'vendors' }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            sourceMap: false
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new CleanPlugin([PATHS.build]),
+        new ExtractTextPlugin('[name].[contenthash:20].css')
+    ]
+})
+
